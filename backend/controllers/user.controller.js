467aa6e6d9d72c1072b2a9e543cd73a5bc490428
TheBoolean-Boy <saswatrath02@gmail.com>
2025-09-05 09:31:06 +0530

    Follow&Unfollow backend logic

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,51 @@
+import User from "../models/user.model.js";
+
+
+export const getUserProfile = async(req, res) =>{
+  const {username} = req.params;
+  try {
+    const user = await User.findOne({username}).select("-password");
+    if(!user){
+      return res.status(400).json({error: "User not found"})
+    }
+
+    res.status(200).json(user)
+  } catch (error) {
+    console.log(`Error in getUserProfile controller: ${error.message}`);
+    res.status(500).json({error: `Internal server error: ${error.message}`})
+  }
+}
+
+export const followUnfollowUser = async(req, res)=> {
+  try {
+    const {id} = req.params;
+    const userToModify = await User.findById(id)
+    const currentUser = await User.findById(req.user._id)
+
+    if(id === req.user._id.toString()){
+      return res.status(400).json({error:"You can't follow/unfollow yourself"})
+    }
+
+    if(!currentUser || !userToModify){
+      return res.status(400).json({error:"User not found"})
+    }
+
+    //Now we check if the user is already following the user
+    const isFollowing = await currentUser.following.includes(id)
+
+    if(isFollowing){
+      //Already following so he will unfollow
+      await User.findByIdAndUpdate(id, {$pull: {followers:req.user._id}})
+      await User.findByIdAndUpdate(req.user._id, {$pull : {following: id}})
+      res.status(200).json({message: "User unfollowed successfully"})
+    }else{
+      //Not following so he will follow
+      await User.findByIdAndUpdate(id, {$push: {followers:req.user._id} })
+      await User.findByIdAndUpdate(req.user._id, { $push: {following: id} })
+      res.status(200).json({message:"User followed successfully"})
+    }
+  } catch (error) {
+    console.log(`Error in getUserProfile controller: ${error.message}`);
+    res.status(500).json({error: `Internal server error: ${error.message}`})
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
-import authRouter from "./routes/auth.routes.js"; 
 import dotenv from "dotenv";
-import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js"; 
+import userRoutes from './routes/user.routes.js'
+import connectDB from "./db/connectDB.js";
 
 
 dotenv.config();
@@ -12,9 +14,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
-app.use("/api/auth", authRouter);
 
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes)
 
 
 app.listen(PORT, () => {
