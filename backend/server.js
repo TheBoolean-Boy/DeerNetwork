3571774f65f267da6a8d34ceae77221a6c96a2ec
TheBoolean-Boy <saswatrath02@gmail.com>
2025-09-08 13:16:37 +0530

    production build ready

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -21,6 +22,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 app.use(express.json({
   limit:'10mb'
@@ -33,6 +35,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notification",notificationRoutes)
+
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
 
 
 app.listen(PORT, () => {
